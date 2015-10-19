c21e9a27920457afa419784bc506bf9a257473fc
Jia Rong <jr.loves.maths@gmail.com>
2015-10-19 23:14:18 +0800

    Animate nav bar.

--- assets/custom-theme.js
+++ assets/custom-theme.js
@@ -3,6 +3,15 @@ jQuery(() => {
   const $ = jQuery;
 
   // Do DOM manipulations here before it is being shown
+  $('.nav-labels').hide();
+
+  $(".fixed-navigation-wrapper").mouseenter(() => {
+    $('.nav-labels').animate({width:'toggle'},350);
+  });
+
+  $(".fixed-navigation-wrapper").mouseleave(() => {
+    $('.nav-labels').animate({width:'toggle'},350);
+  });
 
   // Show DOM
   $('body').fadeIn('fast');
