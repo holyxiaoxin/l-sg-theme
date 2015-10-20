b43d135ca0a538e60f5ea1e487cbb777c6b8290c
Jia Rong <jr.loves.maths@gmail.com>
2015-10-20 11:07:54 +0800

    Fix es6 not working in safari. seriously?

--- assets/custom-theme.js
+++ assets/custom-theme.js
@@ -1,15 +1,14 @@
-jQuery(() => {
-  "use strict";
-  const $ = jQuery;
+jQuery(function() {
+  var $ = jQuery;
 
   // Do DOM manipulations here before it is being shown
   $('.nav-labels').hide();
 
-  $(".fixed-navigation-wrapper").mouseenter(() => {
+  $(".fixed-navigation-wrapper").mouseenter(function() {
     $('.nav-labels').animate({width:'toggle'},350);
   });
 
-  $(".fixed-navigation-wrapper").mouseleave(() => {
+  $(".fixed-navigation-wrapper").mouseleave(function() {
     $('.nav-labels').animate({width:'toggle'},350);
   });
 
@@ -20,10 +19,10 @@ jQuery(() => {
     // Hide fixed-navigation on load
     $('.fixed-navigation-wrapper').hide();
 
-    $(document).scroll(() => {
-      const top = $(document).scrollTop();
-      const viewportHeight = $(window).height();
-      const offsetMargin = 240;
+    $(document).scroll(function() {
+      var top = $(document).scrollTop();
+      var viewportHeight = $(window).height();
+      var offsetMargin = 240;
       if(top>(viewportHeight-offsetMargin)) {
         $('.fixed-navigation-wrapper').fadeIn('fast');
       } else {
